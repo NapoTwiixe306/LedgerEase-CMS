2e040e1704df14053cc6c18d189e2ea88a93d1fc
napotwiixe <julien.milants@gmail.com>
2023-06-13 16:54:53 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Mon CMS Crypto - Accueil</title>
+    <!-- <link rel="stylesheet" href="public/css/style.css"> -->
+</head>
+<body>
+    <?php
+    // Inclure le contenu de la page d'accueil
+    include './app/views/site/Home.php';
+?>
+</body>
+</html>
