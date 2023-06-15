27e5ca73a30dab9baf28d6089448748c133a41f7
napotwiixe <julien.milants@gmail.com>
2023-06-15 15:11:06 +0200

    add comment in siteControllers

---
+++ app/controllers/SiteController.php
@@ -0,0 +1,7 @@
+<?php 
+    require_once '../models.Site.php';
+    // vous pouvez définir une classe de contrôleur qui gère les actions liées aux sites de votre CMS.
+    // Ce contrôleur sera responsable de recevoir les requêtes HTTP,
+    // de traiter les données et d'effectuer les actions appropriées,
+    // telles que la création, la modification ou la suppression d'un site.
+?>
