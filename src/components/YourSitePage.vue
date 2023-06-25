78bcedcbb1076b17c0e6a9161809e90f1d08a27e
Julien <62757437+NapoTwiixe306@users.noreply.github.com>
2023-06-25 17:53:38 +0200

    add bloqued redirect if not log

--- src/components/YourSitePage.vue
+++ src/components/YourSitePage.vue
@@ -1,29 +1,39 @@
 <template>
-    <h1>yop</h1>
-    <button v-if="isLoggedIn" @click="logout">Logout</button>
-</template>
-
-<script>
-import axios from 'axios'
-
-export default {
-  data() {
-    return {
-      isLoggedIn: localStorage.getItem('token') != null, // Vérifier si le token est présent dans le localstorage,
-      name: null
-    };
-  },
-  methods: {
-    async logout() {
-      try {
-        await axios.post('http://localhost:3000/auth/logout'); // Appeler la route de /logout
-        localStorage.removeItem('token'); // Supprimer le token du localStorage
-        this.isLoggedIn = false; // Mettre à jour l'état de connexion
-        this.$router.push('/'); // Rediriger vers la page d'accueil
-      } catch (error) {
-        console.log(error);
+    <div>
+      <h1>yop</h1>
+      <button v-if="isLoggedIn" @click="logout">Logout</button>
+    </div>
+  </template>
+  
+  <script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        isLoggedIn: localStorage.getItem('token') != null,
+        name: null
+      };
+    },
+    created() {
+      // Vérifier si l'utilisateur est connecté lors de la création du composant
+      if (!this.isLoggedIn) {
+        // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
+        this.$router.push('/');
       }
     },
-  }
-};
-</script>
+    methods: {
+      async logout() {
+        try {
+          await axios.post('http://localhost:3000/auth/logout');
+          localStorage.removeItem('token');
+          this.isLoggedIn = false;
+          this.$router.push('/');
+        } catch (error) {
+          console.log(error);
+        }
+      },
+    }
+  };
+  </script>
+  
