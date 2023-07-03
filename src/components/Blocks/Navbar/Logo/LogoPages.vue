f88b99a00a91133bd957ce8511b3a69b985b5eb5
NapoTwiixe <julien.milants@gmail.com>
2023-07-03 20:59:37 +0200

    add all Link scss from NavbarWidget

--- src/components/Blocks/Navbar/Logo/LogoPages.vue
+++ src/components/Blocks/Navbar/Logo/LogoPages.vue
@@ -9,44 +9,9 @@
 </template>
 
 <script>
+import LogoWidget from '../../Js/MoovableWidget.vue.js'
 export default {
-  data() {
-    return {
-      isDragging: false,
-      startPosition: { x: 0, y: 0 },
-      currentPosition: { x: 0, y: 0 },
-      position: { x: 0, y: 0 }
-    };
-  },
-  methods: {
-
-    startDrag(event) {
-      event.preventDefault();
-      this.isDragging = true;
-      this.startPosition.x = event.clientX;
-      this.startPosition.y = event.clientY;
-
-      window.addEventListener('mousemove', this.handleDrag);
-      window.addEventListener('mouseup', this.stopDrag);
-    },
-    handleDrag(event) {
-      if (!this.isDragging) return;
-
-      const deltaX = event.clientX - this.startPosition.x;
-      const deltaY = event.clientY - this.startPosition.y;
-
-      this.currentPosition.x = this.position.x + deltaX;
-      this.currentPosition.y = this.position.y + deltaY;
-    },
-    stopDrag() {
-      this.isDragging = false;
-      this.position.x = this.currentPosition.x;
-      this.position.y = this.currentPosition.y;
-
-      window.removeEventListener('mousemove', this.handleDrag);
-      window.removeEventListener('mouseup', this.stopDrag);
-    }
-  }
+ mixins: [LogoWidget],
 };
 </script>
 
--- src/components/Blocks/Navbar/Searchbar/SearchbarPage.vue
+++ src/components/Blocks/Navbar/Searchbar/SearchbarPage.vue
@@ -15,52 +15,9 @@
 </template>
 
 <script>
+import SearchBarWidget from '../../Js/MoovableWidget.vue.js'
 export default {
-  data() {
-    return {
-      searchQuery: '',
-      isDragging: false,
-      startPosition: { x: 0, y: 0 },
-      currentPosition: { x: 0, y: 0 },
-      position: { x: 0, y: 0 }
-    };
-  },
-  methods: {
-    handleInput() {
-      // Effectuez des actions supplémentaires lors de la saisie de l'utilisateur, si nécessaire
-    },
-    handleSearch() {
-      // Effectuez l'action de recherche en utilisant la valeur de searchQuery
-      // Par exemple, déclenchez une requête API, naviguez vers une page de résultats, etc.
-      console.log('Recherche effectuée:', this.searchQuery);
-    },
-    startDrag(event) {
-      event.preventDefault();
-      this.isDragging = true;
-      this.startPosition.x = event.clientX;
-      this.startPosition.y = event.clientY;
-
-      window.addEventListener('mousemove', this.handleDrag);
-      window.addEventListener('mouseup', this.stopDrag);
-    },
-    handleDrag(event) {
-      if (!this.isDragging) return;
-
-      const deltaX = event.clientX - this.startPosition.x;
-      const deltaY = event.clientY - this.startPosition.y;
-
-      this.currentPosition.x = this.position.x + deltaX;
-      this.currentPosition.y = this.position.y + deltaY;
-    },
-    stopDrag() {
-      this.isDragging = false;
-      this.position.x = this.currentPosition.x;
-      this.position.y = this.currentPosition.y;
-
-      window.removeEventListener('mousemove', this.handleDrag);
-      window.removeEventListener('mouseup', this.stopDrag);
-    }
-  }
+mixins: [SearchBarWidget],
 };
 </script>
 
