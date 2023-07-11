71df961bd95a07fc3fd8c750ded39cab5adb675a
NapoTwiixe <julien.milants@gmail.com>
2023-07-11 14:13:46 +0200

    ReigtserPage.vue finish

--- src/components/Page/RegisterPage.vue
+++ src/components/Page/RegisterPage.vue
@@ -2,35 +2,35 @@
   <Navbar/>
   <div class="register">
     <div class="register2">
-      <h1 class="title">Let's get started</h1>
+      <h1 class="title">{{translations.register.title}}</h1>
       <form @submit.prevent="submitForm">
         <div class="input" >
-          <label class="input-label" for="name">Nom d'utilisateur :</label>
+          <label class="input-label" for="name">{{translations.register.form.userName}}</label>
           <input class="input-field" id="name" type="text" placeholder="Entrez votre nom d'utilisateur ici" v-model="name" required>
         </div>
         <div class="input">
-          <label class="input-label" for="email">Email :</label>
+          <label class="input-label" for="email">{{translations.register.form.Email}}</label>
           <input class="input-field" id="email" type="email" placeholder="Entrez votre adresse email ici" v-model="email" required>
         </div>
         <div class="input">
-          <label class="input-label" for="password">Mot de passe :</label>
+          <label class="input-label" for="password">{{translations.register.form.MDP}}</label>
           <input class="input-field" id="password" type="password" placeholder="Entrez votre mot de passe ici" v-model="password" required>
         </div>
         <div class="action">
           <button class="action-button" type="submit">
-          Créer un compte
+          {{translations.register.ButtonText}}
         </button>
         </div>
       </form>
       <div class="forgot">
-        <span>Vous avez déjà un compte ? </span>
-        <router-link to="/create">Connectez-vous</router-link>
+        <span>{{translations.register.AlwaysAccount}} </span>
+        <router-link to="/create">{{translations.register.log}}</router-link>
       </div>
     </div>
     <span class="vertical-bar"></span>
     <div class="social-log">
-      <a href=""> <font-awesome-icon :icon="['fab', 'google']" class="social"/> Continue With Google</a>
-      <a href=""><font-awesome-icon :icon="['fab', 'github']" class="social"/> Continue With Github</a>
+      <a href=""> <font-awesome-icon :icon="['fab', 'google']" class="social"/>{{translations.register.Social.Google}}</a>
+      <a href=""><font-awesome-icon :icon="['fab', 'github']" class="social"/>{{translations.register.Social.Github}}</a>
     </div>
   </div>
   <Footer/>
@@ -41,16 +41,19 @@ import Navbar from '../Widget/Navbar.vue';
 import Footer from '../Widget/Footer.vue';
 import axios from 'axios'
 import router from '../../router'
+import translations from '@/TextVariables.json'
 export default {
   components: {
     Navbar,
     Footer
+
   },
   data() {
     return {
       name: '',
       email: '',
       password: '',
+      translations: translations,
     }
   },
   methods: {
