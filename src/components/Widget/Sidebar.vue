cd09899440a8002eab9ae4f179b828f080e75790
NapoTwiixe <julien.milants@gmail.com>
2023-07-13 15:45:49 +0200

    optimisation du code

--- src/components/Widget/Sidebar.vue
+++ src/components/Widget/Sidebar.vue
@@ -1,133 +1,111 @@
 <template>
   <div class="sidebar">
     <div class="logo-wrapper">
-      <router-link class="logo" to="/">{{translations.logo}}</router-link>
+      <router-link class="logo" to="/">{{ translations.logo }}</router-link>
     </div>
-    <div class="dropdown">
-      <a href="#" class="dropbtn">{{translations.sidebar.navbar_title}} <font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" /></a>
+    <div class="dropdown" v-for="item in navbarItems" :key="item.title">
+      <a href="#" class="dropbtn">
+        {{ item.title }}
+        <font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" />
+      </a>
       <div class="dropdown-content">
-        <a href="#" @click="showLogo = true">{{translations.sidebar.navbar_item.logo}}</a>
-        <a href="#" @click="showSearchBar = true">{{translations.sidebar.navbar_item.search}}</a>
-        <a href="">{{translations.sidebar.navbar_item.link}}</a>
-        <a href="#" @click="showButtonPage = true">{{translations.sidebar.navbar_item.button}}</a>
-        <a href="#" @click="showTranslatePage = true">{{translations.sidebar.navbar_item.translate}}</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <a href="#" class="dropbtn">{{translations.sidebar.hero_title}}<font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" /></a>
-      <div class="dropdown-content">
-        <a href="">{{translations.sidebar.heros_item.image}}</a>
-        <a href="">{{translations.sidebar.heros_item.video}}</a>
-        <a href="">{{translations.sidebar.heros_item.title}}</a>
-        <a href="">{{translations.sidebar.heros_item.para}}</a>
-        <a href="">{{translations.sidebar.heros_item.icons}}</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <a href="#" class="dropbtn">Header<font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" /></a>
-      <div class="dropdown-content">
-        <a href="">Nombre de Section</a>
-        <a href="">Taille de section</a>
-        <a href="">Titre</a>
-        <a href="">Paragraphe</a>
-        <a href="">Icones</a>
-        <a href="">Image</a>
-        <a href="">Video</a>
-        <a href="">Card Design</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <a href="#" class="dropbtn">À Propos<font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" /></a>
-      <div class="dropdown-content">
-        <a href="">Titre</a>
-        <a href="">Paragraphe</a>
-        <a href="">Icones</a>
-        <a href="">Liens</a>
-        <a href="">Bouton</a>
-      </div>
-    </div>
-    <div class="dropdown">
-      <a href="#" class="dropbtn">Footer <font-awesome-icon :icon="['fas', 'arrow-right']" class="icon-right" /></a>
-      <div class="dropdown-content">
-        <a href="#" @click="showLogo = true">Logo</a>
-        <a href="">Icons</a>
-        <a href="#" @click="showButtonPage = true">Bouton</a>
-        <a href="#">Link</a>
+        <a href="#" v-for="link in item.links" :key="link.title" @click="showComponent(link.component)">{{ link.title }}</a>
       </div>
     </div>
     <div class="dropdown">
       <a href="#" class="dropbtn">DarkMode</a>
     </div>
-    <div class="dropdown">
-      <a href="#" @click="showPalettes = true" class="dropbtn">Palettes de Couleurs</a>
-    </div>
   </div>
-  <div v-if="showSearchBar">
-    <widget-search-bar />
-    <button @click="showSearchBar = false" class="deletebtn">Supprimer</button>
-  </div>
-  <div v-if="showLogo">
-    <WidgetLogo/>
-    <button @click="showLogo = false" class="deletebtn">Supprimer</button>
-  </div>
-  <div v-if="showButtonPage">
-    <ButtonPage/>
-    <button @click="showButtonPage = false" class="deletebtn">Supprimer</button>
-  </div>
-  <div v-if="showTranslatePage">
-    <TranslationPage/>
-    <button @click="showTranslatePage = false" class="deletebtn">Supprimer</button>
-  </div>
-  <div v-if="showPalettes">
-    <ColorsPalettesPage/>
-    <button @click="showPalettes = false" class="deletebtn">Supprimer</button>
+  <div v-for="component in componentsToShow" :key="component">
+    <component :is="component" />
+    <button @click="hideComponent(component)" class="deletebtn">Supprimer</button>
   </div>
 </template>
 
 <script>
-import WidgetSearchBar from "@/components/Blocks/Navbar/Searchbar/SearchbarPage.vue";
-import WidgetLogo from "@/components/Blocks/Navbar/Logo/LogoPages.vue";
+import translations from "@/TextVariables.json";
+
+import SearchBar from "@/components/Blocks/Navbar/Searchbar/SearchbarPage.vue";
+import Logo from "@/components/Blocks/Navbar/Logo/LogoPages.vue";
 import ButtonPage from "@/components/Blocks/Navbar/Button/ButtonPage.vue";
-import translations from "@/TextVariables.json"
 import TranslationPage from "@/components/Blocks/Navbar/Translation/TranslationPage.vue";
 import ColorsPalettesPage from "@/components/Blocks/ColorsPalettes/ColorsPalettesPage.vue";
+
 export default {
   name: 'AppSidebar',
   components: {
-    ColorsPalettesPage,
-    TranslationPage,
+    SearchBar,
+    Logo,
     ButtonPage,
-    WidgetSearchBar,
-    WidgetLogo,
+    TranslationPage,
+    ColorsPalettesPage,
   },
   data() {
     return {
-      showSearchBar: false,
-      showLogo: false,
-      showButtonPage: false,
-      showTranslatePage: false,
-      showPalettes: false,
-      translations: translations
-
+      componentsToShow: [],
+      navbarItems: [
+        {
+          title: translations.sidebar.navbar_title,
+          links: [
+            { title: translations.sidebar.navbar_item.logo, component: 'Logo' },
+            { title: translations.sidebar.navbar_item.search, component: 'SearchBar' },
+            { title: translations.sidebar.navbar_item.link },
+            { title: translations.sidebar.navbar_item.button, component: 'ButtonPage' },
+            { title: translations.sidebar.navbar_item.translate, component: 'TranslationPage' },
+          ],
+        },
+        {
+          title: translations.sidebar.header_title,
+          links: [
+            { title: translations.sidebar.header_item.sectionNumber },
+            { title: translations.sidebar.header_item.sectionSize },
+            { title: translations.sidebar.header_item.title },
+            { title: translations.sidebar.header_item.para },
+            { title: translations.sidebar.header_item.icons },
+            { title: translations.sidebar.header_item.img },
+            { title: translations.sidebar.header_item.video },
+            { title: translations.sidebar.header_item.card },
+          ],
+        },
+        {
+          title: translations.sidebar.about_title,
+          links: [
+            { title: translations.sidebar.about_item.title },
+            { title: translations.sidebar.about_item.para },
+            { title: translations.sidebar.about_item.icons },
+            { title: translations.sidebar.about_item.link },
+            { title: translations.sidebar.about_item.button, component: ButtonPage },
+          ],
+        },
+        {
+          title: 'Footer',
+          links: [
+            { title: translations.sidebar.footer_item.logo, component: 'Logo' },
+            { title: translations.sidebar.footer_item.icons},
+            { title: translations.sidebar.footer_item.button, component: 'ButtonPage' },
+            { title: translations.sidebar.footer_item.link },
+          ],
+        },
+        {
+          title: translations.sidebar.colors_title,
+          links: [
+            { title: 'Palette de Couleurs', component: 'ColorsPalettesPage' },
+          ],
+        },
+      ],
+      translations: translations,
     };
   },
   methods: {
-    hideSearchBar() {
-      this.showSearchBar = false;
+    showComponent(component) {
+      this.componentsToShow.push(component);
     },
-    hideLogo(){
-      this.showLogo = false;
+    hideComponent(component) {
+      const index = this.componentsToShow.indexOf(component);
+      if (index !== -1) {
+        this.componentsToShow.splice(index, 1);
+      }
     },
-    hideButtonPage(){
-      this.showButtonPage = false;
-    },
-    hideTranslatePage(){
-      this.showTranslatePage = false;
-    },
-    hidePalettes(){
-      this.showPalettes = false;
-    }
-
   },
 };
 </script>
